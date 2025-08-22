1777969cefe9a95dabf22478e39b4cd607bc8816
JaeYoungJung <hanol98@naver.com>
2025-08-22 14:47:19 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/phoenix/planet/departmentcorebackend/DepartmentCoreBackendApplication.java
@@ -0,0 +1,14 @@
+package org.phoenix.planet.departmentcorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentCoreBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DepartmentCoreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/phoenix/planet/departmentcorebackend/DepartmentCoreBackendApplicationTests.java
@@ -0,0 +1,14 @@
+package org.phoenix.planet.departmentcorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentCoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
